7a8bb059789dfe961fcacddfb118d006843fdcd1
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-20 16:54:58 +0800

    Update cilium.sh

--- cilium.sh
+++ cilium.sh
@@ -11,3 +11,11 @@ helm install cilium cilium/cilium --version 1.14.1 \
    --set hostPort.enabled=true \
    --set cluster.name=c1 \
    --set cluster.id=1
+
+
+curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+rm cilium-linux-amd64.tar.gz{,.sha256sum}
+cilium version
+
