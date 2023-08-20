71a5590c31d63bbdd819251fb583ea1ab2aa3016
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-20 17:54:07 +0800

    Create skupper.sh

---
+++ skupper.sh
@@ -0,0 +1,4 @@
+
+### https://skupper.io/docs/cli/index.html#installing-cli
+
+curl -sSL https://github.com/skupperproject/skupper/releases/download/1.4.2/skupper-cli-1.4.2-linux-amd64.tgz | sudo tar xz -C /usr/local/bin skupper
