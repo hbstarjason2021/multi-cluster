bef5c600ebf6a61e26abac43aa2060d0b3823aec
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-20 16:50:36 +0800

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,11 +1,17 @@
 
 ### Install KIND
-curl -Lo /usr/bin/kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64 && chmod +x /usr/bin/kind
+KIND_VESION="v0.20.0"
+curl -Lo /usr/bin/kind https://kind.sigs.k8s.io/dl/${KIND_VESION}/kind-linux-amd64 && chmod +x /usr/bin/kind
 kind version 
 
 ### Install Kubectl 
 curl -L -o /usr/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x /usr/bin/kubectl
-kubectl version 
+kubectl version --client
+
+### Install kubecolor
+KUBECOLOR_VERSION=0.0.25
+curl -sSL https://github.com/hidetatz/kubecolor/releases/download/v${KUBECOLOR_VERSION}/kubecolor_${KUBECOLOR_VERSION}_Linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin kubecolor
+kubecolor version --client
 
 ### Install Helm 
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
