cee2559d5950e9abe11d94e541450d8f999c1d69
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-22 17:57:58 +0800

    Create 03-hal.sh

---
+++ 03-hal.sh
@@ -0,0 +1,6 @@
+####
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh && \
+    bash InstallHalyard.sh -y  && \
+    hal -v 
+
+ hal version list    
