883b15ad92837db43a58b063b1038cfbcb9eb722
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-23 11:12:30 +0800

    Update 02-k3s.sh

--- 02-k3s.sh
+++ 02-k3s.sh
@@ -12,9 +12,12 @@ cp /etc/rancher/k3s/k3s.yaml  ~/.kube/config
 kubectl get node 
 
 
-### 国内安装k3s
+########################### ### 国内安装k3s
+:<<COMMENT
+
 #curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
-######
+
+
 curl –sfL \
      https://rancher-mirror.oss-cn-beijing.aliyuncs.com/k3s/k3s-install.sh | \
      INSTALL_K3S_MIRROR=cn sh -s - \
@@ -22,3 +25,15 @@ curl –sfL \
      --write-kubeconfig ~/.kube/config \
      --write-kubeconfig-mode 666 \
      --disable traefik
+
+
+cat > /etc/rancher/k3s/registries.yaml <<EOF
+mirrors:
+  docker.io:
+    endpoint:
+      - "http://hub-mirror.c.163.com"
+      - "https://docker.mirrors.ustc.edu.cn"
+      - "https://registry.docker-cn.com"
+EOF
+
+COMMENT
