82d38adf25add04a9dc0a50a37375c032128b978
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-24 10:42:22 +0800

    Update kind.sh

--- kind.sh
+++ kind.sh
@@ -3,6 +3,10 @@ kind create cluster --name c1 --config kind-cluster-c1.yaml
 
 kubectl cluster-info --context kind-c1
 
+kubectl config use-context kind-c1
+
+
+
 # kind create cluster --name c2 --config kind-cluster-c2.yaml
 
 ## 
