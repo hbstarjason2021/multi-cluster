50f406b85d4c35a49ba46c2fc3969c3e8bdfcb1a
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-26 19:38:22 +0800

    Update metallb.sh

--- metallb.sh
+++ metallb.sh
@@ -11,11 +11,28 @@ sleep 10
 kubectl wait --namespace metallb-system --for=condition=ready pod --selector=app=metallb --timeout=30s
 
 
-
+# setup MetalLB IP address pool
+# Get the subnet from Docker
 subnet=$(docker network inspect kind | jq -r '.[].IPAM.Config[].Subnet' | grep -P '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/.*$')
 
+# Split the subnet into the base IP and the CIDR suffix
+IFS='/' read -r ip cidr <<< "$subnet"
 
-###############
+# Convert the base IP to a 32-bit integer
+IFS='.' read -r i1 i2 i3 i4 <<< "$ip"
+ip_dec=$(( (i1<<24) + (i2<<16) + (i3<<8) + i4 ))
+
+# Calculate the number of hosts
+num_hosts=$(( 2 ** (32 - cidr) ))
+
+# Calculate the start and end IPs for the second half
+start_ip_dec=$(( ip_dec + num_hosts / 2 ))
+end_ip_dec=$(( ip_dec + num_hosts - 1 ))
+
+# Convert the start and end IPs back to dotted decimal format
+start_ip=$(printf "%d.%d.%d.%d" $(( (start_ip_dec & 0xFF000000) >> 24 )) $(( (start_ip_dec & 0x00FF0000) >> 16 )) $(( (start_ip_dec & 0x0000FF00) >> 8 )) $(( start_ip_dec & 0x000000FF )))
+end_ip=$(printf "%d.%d.%d.%d" $(( (end_ip_dec & 0xFF000000) >> 24 )) $(( (end_ip_dec & 0x00FF0000) >> 16 )) $(( (end_ip_dec & 0x0000FF00) >> 8 )) $(( end_ip_dec & 0x000000FF )))
+
 cat <<EOF| kubectl apply -f -
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
