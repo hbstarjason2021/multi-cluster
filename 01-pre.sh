a5e0b57335dd9e9bc448c7a9738d7bdba2449911
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-29 16:13:57 +0800

    Update 01-pre.sh

--- 01-pre.sh
+++ 01-pre.sh
@@ -97,5 +97,5 @@ curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/d
 sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
 sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
 rm cilium-linux-amd64.tar.gz{,.sha256sum}
-cilium version
+# cilium version
 
