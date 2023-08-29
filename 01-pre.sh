5acf7166406cb78c152f2ddb7b9f473aa93cf5b2
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-08-29 16:24:17 +0800

    Update 01-pre.sh

--- 01-pre.sh
+++ 01-pre.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eux
 
+########   bash <(curl -Ls https://ghproxy.com/https://raw.githubusercontent.com/hbstarjason2021/multi-cluster/main/01-pre.sh)
+########   bash <(curl -Ls https://raw.githubusercontent.com/hbstarjason2021/multi-cluster/main/01-pre.sh)
+
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 function getLocation() {
@@ -36,6 +40,39 @@ fi
 kubectl version --client
 }
 
+###### Install Kubectx
+function install_kubectx() {
+
+KUBECTX_VERSION="v0.9.5"
+# KUBECTX_VERSION=$(curl -s https://api.github.com/repos/ahmetb/kubectx/releases | jq -r '.[0].tag_name')
+# KUBECTX_VERSION=$(curl -s -N https://api.github.com/repos/ahmetb/kubectx/releases | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | head -n 1)
+
+if [ "$countryCode" == "CN" ]; then
+echo -e "检测到国内环境，正在使用镜像下载"
+  curl  https://jihulab.com/hbstarjason/ali-init/-/raw/main/kubectx_v0.9.5_linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin kubectx
+else
+  curl -sSL https://github.com/ahmetb/kubectx/releases/download/${KUBECTX_VERSION}/kubectx_${KUBECTX_VERSION}_linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin kubectx
+fi
+
+## apt install jq -y
+
+}
+
+### Install Helm 
+function install_kubectx() {
+
+if [ "$countryCode" == "CN" ]; then
+echo -e "检测到国内环境，正在使用镜像下载"
+  curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+else
+  curl -sSL https://jihulab.com/hbstarjason/ali-init/-/raw/main/helm-v3.11.3-linux-amd64.tar.gz | sudo tar xz -C /usr/local/bin --strip-components=1 linux-amd64/helm
+fi
+
+helm version 
+}
+
+
+
 ###### Install nexttrace
 ## https://mtr.moe/README_zh_CN.html
 
@@ -54,34 +91,21 @@ getLocation
 
 install_kind
 install_kubectl
+install_kubectx
+install_helm
+
 install_nexttrace
-
-
 
 
 ### Install kubecolor
 KUBECOLOR_VERSION="0.0.25"
 # KUBECOLOR_VERSION=$(curl -s -N https://api.github.com/repos/hidetatz/kubecolor/releases | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | head -n 1)
 
-latestURL=$(curl -s https://api.github.com/repos/hidetatz/kubecolor/releases/latest | grep -i "browser_download_url.*${osDistribution}.*${archParam}" | awk -F '"' '{print $4}')
+# latestURL=$(curl -s https://api.github.com/repos/hidetatz/kubecolor/releases/latest | grep -i "browser_download_url.*${osDistribution}.*${archParam}" | awk -F '"' '{print $4}')
 
 curl -sSL https://github.com/hidetatz/kubecolor/releases/download/v${KUBECOLOR_VERSION}/kubecolor_${KUBECOLOR_VERSION}_Linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin kubecolor
 kubecolor version --client
 
-### Install kubectx
-## apt install jq -y
-
-KUBECTX_VERSION="v0.9.5"
-# KUBECTX_VERSION=$(curl -s https://api.github.com/repos/ahmetb/kubectx/releases | jq -r '.[0].tag_name')
-# KUBECTX_VERSION=$(curl -s -N https://api.github.com/repos/ahmetb/kubectx/releases | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | head -n 1)
-
-curl -sSL https://github.com/ahmetb/kubectx/releases/download/${KUBECTX_VERSION}/kubectx_${KUBECTX_VERSION}_linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin kubectx
-# curl  https://jihulab.com/hbstarjason/ali-init/-/raw/main/kubectx_v0.9.5_linux_x86_64.tar.gz | sudo tar xz -C /usr/local/bin kubectx
-
-### Install Helm 
-curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-# curl -sSL https://jihulab.com/hbstarjason/ali-init/-/raw/main/helm-v3.11.3-linux-amd64.tar.gz | sudo tar xz -C /usr/local/bin --strip-components=1 linux-amd64/helm
-helm version 
 
 ### Install terraform
 TF_VERSION="1.5.6"
