3aad23cde39c671428c7bb3708022a31dbd5bdce
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-09-19 09:30:41 +0800

    Update 04-hal.sh

--- 04-hal.sh
+++ 04-hal.sh
@@ -1,4 +1,11 @@
 ####
+
+ROOT_DIR="$(
+  cd "$(dirname "$0")" >/dev/null 2>&1 || exit 1
+  pwd -P
+)"
+
+
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh && \
     bash InstallHalyard.sh -y  && \
     hal -v 
