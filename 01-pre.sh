15e568c7cfd6038b2df645172e0aca611a748545
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2024-04-14 18:50:08 +0800

    Update 01-pre.sh

--- 01-pre.sh
+++ 01-pre.sh
@@ -115,7 +115,7 @@ echo -e "${green}Helm is already installed${white}"
 
 ### Install terraform
 function install_terraform() {
-TF_VERSION="1.5.6"
+TF_VERSION="1.8.0"
 
 ## TF_VERSION=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
 ## wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip
